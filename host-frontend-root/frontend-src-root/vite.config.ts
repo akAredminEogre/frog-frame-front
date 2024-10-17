cb3809df3937f72090d2e90a4acda19e4902a2bd
akAredminEogre <32060459+akAredminEogre@users.noreply.github.com>
2024-10-18 04:38:35 +0900

    draft:

--- host-frontend-root/frontend-src-root/vite.config.ts
+++ host-frontend-root/frontend-src-root/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
   server: {
     host: '0.0.0.0', // サーバーのバインドアドレスを設定
     port: 5173,      // ポート番号を設定（必要に応じて変更）
