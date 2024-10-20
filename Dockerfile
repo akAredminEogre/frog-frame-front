02be3d1abc5cda85cf921d7cf3602db01241af92
akAredminEogre <32060459+akAredminEogre@users.noreply.github.com>
2024-10-21 07:58:00 +0900

    feat: phase 04

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
 FROM node:23.0-alpine3.19
 
-WORKDIR /opt/frontend-container-app-root/
+WORKDIR /opt/frontend-container-app-root/frontend-src-root
+
+COPY ./host-frontend-root/frontend-src-root /opt/frontend-container-app-root/frontend-src-root
+
+RUN npm install
+
+CMD ["npm", "run", "dev"]
--- host-frontend-root/frontend-src-root/vite.config.ts
+++ host-frontend-root/frontend-src-root/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // サーバーのバインドアドレスを設定
+    port: 5173,      // ポート番号を設定（必要に応じて変更）
+    strictPort: true, // ポートが既に使用されている場合にエラーをスロー
+    hmr: {
+      port: 5173,    // HMR が使用するポートを設定
+    }
+  }
 })
+
