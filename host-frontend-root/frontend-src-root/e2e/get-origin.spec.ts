3c33d77d6b74a7a6c76ce55938214997a7324df8
akAredminEogre <32060459+akAredminEogre@users.noreply.github.com>
2025-08-05 09:44:52 +0900

    test: URL自動入力のe2eテスト

--- host-frontend-root/frontend-src-root/e2e/get-origin.spec.ts
+++ host-frontend-root/frontend-src-root/e2e/get-origin.spec.ts
@@ -1,15 +1,17 @@
 import { test, expect } from './fixtures';
 
-test('ポップアップを開くと、URLパターンのフォームにブラウザに表示されているページのURLのドメインが自動入力されている', async ({  }) => {
-  // Arrange: Load the extension and navigate to a page where the content script runs.   await page.goto('https://example.com');
-  
-  // Act:   
-  // 1. Open the extension popup
-  // const popup = await browser.newPage();
-  // await popup.goto(`chrome-extension://${extensionId}/popup.html`);   
-  
-  // Assert: popup text has 'example.com'
-  // await expect(popup.locator('body')).toContainText('example.com');
+test('ポップアップを開くと、URLパターンのフォームにブラウザに表示されているページのURLのドメインが自動入力されている', async ({ page, context, popupPage }) => {
 
+  // メインページに移動し、フォーカスを当てる
+  await page.goto('https://agilemanifesto.org/iso/ja/manifesto.html');
+  await page.bringToFront();
+
+  //  ポップアップをリロードして、最新のアクティブタブ情報を取得させる
+  await popupPage.reload();
+
+  // Assert
+  const urlPatternInput = popupPage.getByLabel('URLパターン (前方一致):');
   
+  // 値が設定されるまで、デフォルトより長いタイムアウトを設定して待機
+  await expect(urlPatternInput).toHaveValue('https://agilemanifesto.org', { timeout: 10000 });
 });
