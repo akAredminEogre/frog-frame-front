6237b49b0e9d25eedcec66381714405caeb887fd
akAredminEogre <akAredminEogre@gmail.com>
2025-08-27 14:33:42 +0900

    refactor: 不要になった変数、メソッドと、そのテストを削除

--- host-frontend-root/frontend-src-root/src/domain/value-objects/WorkingHtml.ts
+++ host-frontend-root/frontend-src-root/src/domain/value-objects/WorkingHtml.ts
@@ -1,6 +1,5 @@
 import { TextRange } from './TextRange';
 import { RewriteRule } from '../entities/RewriteRule';
-import { NormalizedString } from './NormalizedString';
 
 /**
  * 置換処理中のHTML文字列を表すValue Object
@@ -9,12 +8,10 @@ import { NormalizedString } from './NormalizedString';
 export class WorkingHtml {
   private readonly value: string;
   private readonly rule: RewriteRule;
-  private readonly normalizedOldString: NormalizedString;
 
   constructor(value: string, rule: RewriteRule) {
     this.value = value;
     this.rule = rule;
-    this.normalizedOldString = new NormalizedString(rule.oldString);
   }
 
   /**
@@ -79,56 +76,5 @@ export class WorkingHtml {
     return new RegExp(redundantPattern);
   }
 
-  /**
-   * 正規化されたインデックスから実際のHTML文字列内のインデックスを取得
-   * @param normalizedIndex 正規化された文字列でのインデックス
-   * @returns 実際のHTML文字列内のインデックス
-   */
-  findActualIndexFromNormalizedIndex(normalizedIndex: number): number {
-    let actualIndex = 0;
-    let currentNormalizedIndex = 0;
-    
-    while (currentNormalizedIndex < normalizedIndex && actualIndex < this.value.length) {
-      // '<'の前の空白をスキップ
-      if (this.isWhitespaceBeforeTag(actualIndex)) {
-        actualIndex++;
-        continue;
-      }
-      
-      // '>'の後の空白をスキップ
-      if (this.isWhitespaceAfterTag(actualIndex)) {
-        actualIndex++;
-        continue;
-      }
-      
-      // 通常の文字はカウントして進む
-      currentNormalizedIndex++;
-      actualIndex++;
-    }
-    
-    return actualIndex;
-  }
-
-  /**
-   * 指定されたインデックスから始まる2文字が'空白+<'のパターンかを判定
-   * @param index 判定開始位置のインデックス
-   * @returns boolean 空白+<のパターンの場合はtrue
-   */
-  isWhitespaceBeforeTag(index: number): boolean {
-    if (index + 1 >= this.value.length) return false;
-    const twoLetters = this.value.substring(index, index + 2);
-    return /\s</.test(twoLetters);
-  }
-
-  /**
-   * 指定されたインデックスの前の文字から2文字が'>+空白'のパターンかを判定
-   * @param index 判定対象位置のインデックス
-   * @returns boolean >+空白のパターンの場合はtrue
-   */
-  isWhitespaceAfterTag(index: number): boolean {
-    if (index <= 0) return false;
-    const twoLetters = this.value.substring(index - 1, index + 1);
-    return />\s/.test(twoLetters);
-  }
 
 }
