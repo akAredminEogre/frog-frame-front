7474a68d0869a1369f18a24373166eead916ca20
akAredminEogre <akAredminEogre@gmail.com>
2025-08-30 15:27:20 +0900

    refactor: NodeTextReplacerの簡素化

--- host-frontend-root/frontend-src-root/src/domain/entities/NodeTextReplacer.ts
+++ host-frontend-root/frontend-src-root/src/domain/entities/NodeTextReplacer.ts
@@ -1,27 +1,16 @@
 import { RewriteRule } from './RewriteRule';
-import { ReplacementValue } from '../value-objects/ReplacementValue';
-import { TextReplacer } from './TextReplacer';
 import { HtmlReplacer } from './HtmlReplacer';
 
 export class NodeTextReplacer {
-  private textReplacer: TextReplacer;
   private htmlReplacer: HtmlReplacer;
 
   constructor(
-    textReplacer: TextReplacer = new TextReplacer(),
     htmlReplacer: HtmlReplacer = new HtmlReplacer(),
   ) {
-    this.textReplacer = textReplacer;
     this.htmlReplacer = htmlReplacer;
   }
 
   replace(root: Node, rule: RewriteRule): void {
-    const replacementValue = new ReplacementValue(rule.oldString);
-
-    if (replacementValue.isHtml()) {
-      this.htmlReplacer.replace(root, rule);
-    } else {
-      this.textReplacer.replace(root, rule);
-    }
+    this.htmlReplacer.replace(root, rule);
   }
 }
