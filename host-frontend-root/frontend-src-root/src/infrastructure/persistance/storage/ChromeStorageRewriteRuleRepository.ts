2ea5a62d8c1aa333ed2564855792cd7fb5f4576e
akAredminEogre <akaredmineogre@gmail.com>
2025-09-11 19:55:18 +0900

    feat: 永続化処理の実装

---
+++ host-frontend-root/frontend-src-root/src/infrastructure/persistance/storage/ChromeStorageRewriteRuleRepository.ts
@@ -0,0 +1,8 @@
+import { IRewriteRuleRepository } from 'src/application/ports/IRewriteRuleRepository';
+import { RewriteRule } from 'src/domain/entities/RewriteRule';
+
+export class ChromeStorageRewriteRuleRepository implements IRewriteRuleRepository {
+  async save(rule: RewriteRule): Promise<void> {
+    await chrome.storage.local.set({ [rule.id]: rule });
+  }
+}
