922ab67cd56b091648a5955ac455eb7bf01411e6
akAredminEogre <akaredmineogre@gmail.com>
2025-09-13 08:00:21 +0900

    refactor: 現在のタブのURL情報を削除

--- host-frontend-root/frontend-src-root/src/domain/value-objects/CurrentTab.ts
+++ host-frontend-root/frontend-src-root/src/domain/value-objects/CurrentTab.ts
@@ -1,17 +1,10 @@
-import { CurrentUrl } from 'src/domain/value-objects/CurrentUrl';
 import { TabId } from 'src/domain/value-objects/TabId';
 
 export class CurrentTab {
   private readonly _tabId: TabId;
-  private readonly _currentUrl: CurrentUrl;
 
-  constructor(currentUrl: string, tabId: number) {
-    this._currentUrl = new CurrentUrl(currentUrl);
+  constructor( tabId: number) {
     this._tabId = new TabId(tabId);
-  }
-
-  get currentUrl(): string {
-    return this._currentUrl.value;
   }
 
   get tabId(): number {
