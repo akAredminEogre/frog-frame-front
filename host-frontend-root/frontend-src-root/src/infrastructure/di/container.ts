40c9c31e2d15278bcc95394ae9505401555565ba
akAredminEogre <32060459+akAredminEogre@users.noreply.github.com>
2025-09-16 09:23:47 +0900

    feat: DIコンテナに新たなクラスを追加

--- host-frontend-root/frontend-src-root/src/infrastructure/di/container.ts
+++ host-frontend-root/frontend-src-root/src/infrastructure/di/container.ts
@@ -9,12 +9,18 @@ import { HandleContextMenuReplaceDomElement } from 'src/application/usecases/con
 import { ContextMenuSetupUseCase } from 'src/application/usecases/contextmenu/ContextMenuSetupUseCase';
 import { HandleStorageChangedUseCase } from 'src/application/usecases/rule/HandleStorageChangedUseCase';
 import { ChromeTabsService } from 'src/infrastructure/browser/tabs/ChromeTabsService';
+import { SelectedPageTextService } from 'src/infrastructure/persistance/storage/SelectedPageTextService';
+import { ChromePopupService } from 'src/infrastructure/browser/popup/ChromePopupService';
 import { IChromeTabsService } from 'src/application/ports/IChromeTabsService';
+import { ISelectedPageTextService } from 'src/application/ports/ISelectedPageTextService';
+import { IPopupService } from 'src/application/ports/IPopupService';
 
 // Register implementations for interfaces (抽象化のため)
 container.register<IChromeTabsService>('IChromeTabsService', { useClass: ChromeTabsService });
+container.register<ISelectedPageTextService>('ISelectedPageTextService', { useClass: SelectedPageTextService });
+container.register<IPopupService>('IPopupService', { useClass: ChromePopupService });
 
-// Register concrete classes (@injectable デコレーターがあれば自動解決されるが、明示性のため記載)
+// Register concrete classes (required for container.resolve() to work)
 container.register(HandleContextMenuReplaceDomElement, { useClass: HandleContextMenuReplaceDomElement });
 container.register(ContextMenuSetupUseCase, { useClass: ContextMenuSetupUseCase });
 container.register(HandleStorageChangedUseCase, { useClass: HandleStorageChangedUseCase });
