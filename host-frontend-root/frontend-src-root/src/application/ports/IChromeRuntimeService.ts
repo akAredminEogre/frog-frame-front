3791d2e733994e277914114cdb285c45b5e8d489
akAredminEogre <akaredmineogre@gmail.com>
2025-09-18 05:33:24 +0900

    refactor: chrome runtimeサービスの引数を変更

--- host-frontend-root/frontend-src-root/src/application/ports/IChromeRuntimeService.ts
+++ host-frontend-root/frontend-src-root/src/application/ports/IChromeRuntimeService.ts
@@ -1,5 +1,5 @@
-import { CurrentTab } from 'src/domain/value-objects/CurrentTab';
+import { TabId } from 'src/domain/value-objects/TabId';
 
 export interface IChromeRuntimeService {
-  sendApplyRewriteRuleMessage(currentTab: CurrentTab): Promise<{ success: boolean; error?: string }>;
+  sendApplyRewriteRuleMessage(tabId: TabId): Promise<{ success: boolean; error?: string }>;
 }
