7e91322117a2101a83afca79c83f1eab1b0e850a
akAredminEogre <akaredmineogre@gmail.com>
2025-09-18 06:24:23 +0900

    refactor: メッセージ関連の引数はプリミティブ型で渡す

--- host-frontend-root/frontend-src-root/src/infrastructure/browser/tabs/ChromeTabsService.ts
+++ host-frontend-root/frontend-src-root/src/infrastructure/browser/tabs/ChromeTabsService.ts
@@ -1,15 +1,14 @@
 import { injectable } from 'tsyringe';
 import { IChromeTabsService } from 'src/application/ports/IChromeTabsService';
-import { TabId } from 'src/domain/value-objects/TabId';
 
 /**
  * Chrome Tabs APIを使用して現在のタブにメッセージを送信するサービスの実装
  */
 @injectable()
 export class ChromeTabsService implements IChromeTabsService {
-  async sendMessage(tabId: TabId, message: any): Promise<any> {
+  async sendMessage(tabId: number, message: any): Promise<any> {
     try {
-      const response = await chrome.tabs.sendMessage(tabId.value, message);
+      const response = await chrome.tabs.sendMessage(tabId, message);
       return response;
     } catch (error) {
       console.error('[ChromeTabsService] sendMessage error:', error);
