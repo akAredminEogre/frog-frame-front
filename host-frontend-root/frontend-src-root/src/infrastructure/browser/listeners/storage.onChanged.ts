7933d2c19d546ef277144e27532cd9e177460c8a
akAredminEogre <akaredmineogre@gmail.com>
2025-09-22 16:01:40 +0900

    refactor: registerStorageOnChangedを廃止

--- host-frontend-root/frontend-src-root/src/infrastructure/browser/listeners/storage.onChanged.ts
+++
@@ -1,9 +0,0 @@
-import { container } from 'src/infrastructure/di/container';
-import { HandleStorageChangedUseCase } from 'src/application/usecases/rule/HandleStorageChangedUseCase';
-
-export function registerStorageOnChanged() {
-  chrome.storage.onChanged.addListener((changes, namespace) => {
-    const handleStorageChangedUseCase = container.resolve(HandleStorageChangedUseCase);
-    handleStorageChangedUseCase.execute(changes, namespace);
-  });
-}
