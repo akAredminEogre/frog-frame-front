0c112e02c1c667c63cf0b05aceb74e9b0c2f9a46
akAredminEogre <akaredmineogre@gmail.com>
2025-09-26 09:23:50 +0900

    refactor: entrypointsディレクトリ移動の影響を修正

--- host-frontend-root/frontend-src-root/wxt.config.ts
+++ host-frontend-root/frontend-src-root/wxt.config.ts
@@ -8,6 +8,7 @@ const devPort = process.env.WXT_DEV_PORT ? parseInt(process.env.WXT_DEV_PORT, 10
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  srcDir: 'src',
   manifest: {
     permissions: ['contextMenus', 'storage', 'tabs', 'scripting'],
     host_permissions: ['<all_urls>']
