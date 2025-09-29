34d7491dd595e8263eeba1747eac48409412587a
akAredminEogre <akaredmineogre@gmail.com>
2025-09-29 15:13:58 +0900

    feat: tabIDから、現在のタブ情報を取得するロジック(interface)

--- host-frontend-root/frontend-src-root/src/application/ports/ICurrentTabService.ts
+++ host-frontend-root/frontend-src-root/src/application/ports/ICurrentTabService.ts
@@ -1,5 +1,7 @@
 import { CurrentTab } from 'src/domain/value-objects/CurrentTab';
+import { TabId } from 'src/domain/value-objects/TabId';
 
 export interface ICurrentTabService {
   getCurrentTab(): Promise<CurrentTab>;
+  getTabById(tabId: TabId): Promise<CurrentTab>;
 }
