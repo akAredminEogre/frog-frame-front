73fd7f37b32582bfad4b1bbca531601ae1d89b08
akAredminEogre <akaredmineogre@gmail.com>
2025-10-10 05:13:48 +0900

    refactor: interfaceではなく、Tab型のValueObjectを利用する

--- host-frontend-root/frontend-src-root/src/application/ports/IChromeTabsService.ts
+++ host-frontend-root/frontend-src-root/src/application/ports/IChromeTabsService.ts
@@ -1,7 +1,4 @@
-export interface Tab {
-  id?: number;
-  url?: string;
-}
+import { Tab } from 'src/domain/value-objects/Tab';
 
 export interface IChromeTabsService {
   sendMessage(tabId: number, message: any): Promise<any>;
