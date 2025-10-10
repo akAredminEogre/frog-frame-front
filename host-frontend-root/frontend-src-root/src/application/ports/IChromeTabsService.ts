65042aa37fbb449b03eec34aaa202f0cabda5798
akAredminEogre <32060459+akAredminEogre@users.noreply.github.com>
2025-10-10 11:48:57 +0900

    feat: queryTabsの返り値はTabs型であることを明確化

--- host-frontend-root/frontend-src-root/src/application/ports/IChromeTabsService.ts
+++ host-frontend-root/frontend-src-root/src/application/ports/IChromeTabsService.ts
@@ -1,6 +1,6 @@
-import { Tab } from 'src/domain/value-objects/Tab';
+import { Tabs } from 'src/domain/value-objects/Tabs';
 
 export interface IChromeTabsService {
   sendMessage(tabId: number, message: any): Promise<any>;
-  queryTabs(queryInfo: any): Promise<Tab[]>;
+  queryTabs(queryInfo: any): Promise<Tabs>;
 }
