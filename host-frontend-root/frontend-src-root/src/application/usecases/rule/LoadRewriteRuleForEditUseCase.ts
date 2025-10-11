dc49245e8de47fb3b3affde1e4f1694b9fe2c45a
akAredminEogre <32060459+akAredminEogre@users.noreply.github.com>
2025-10-12 06:24:20 +0900

    refactor: DIコンテナを利用しコードを簡素化

--- host-frontend-root/frontend-src-root/src/application/usecases/rule/LoadRewriteRuleForEditUseCase.ts
+++ host-frontend-root/frontend-src-root/src/application/usecases/rule/LoadRewriteRuleForEditUseCase.ts
@@ -1,8 +1,11 @@
+import { injectable, inject } from 'tsyringe';
 import { IRewriteRuleRepository } from 'src/application/ports/IRewriteRuleRepository';
 import { RewriteRule } from 'src/domain/entities/RewriteRule/RewriteRule';
 
+@injectable()
 export class LoadRewriteRuleForEditUseCase {
   constructor(
+    @inject('IRewriteRuleRepository')
     private readonly rewriteRuleRepository: IRewriteRuleRepository
   ) {}
 
