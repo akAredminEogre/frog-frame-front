f2e3c0401ee3a99cef1efb8041a176c552f4647f
akAredminEogre <32060459+akAredminEogre@users.noreply.github.com>
2025-10-14 08:31:08 +0900

    feat: タブを閉じるユースケースの実装

---
+++ host-frontend-root/frontend-src-root/src/application/usecases/window/CloseCurrentWindowUseCase.ts
@@ -0,0 +1,14 @@
+import { injectable, inject } from 'tsyringe';
+import { IWindowService } from 'src/application/ports/IWindowService';
+
+@injectable()
+export class CloseCurrentWindowUseCase {
+  constructor(
+    @inject('IWindowService')
+    private readonly windowService: IWindowService
+  ) {}
+
+  async execute(): Promise<void> {
+    await this.windowService.closeCurrentWindow();
+  }
+}
