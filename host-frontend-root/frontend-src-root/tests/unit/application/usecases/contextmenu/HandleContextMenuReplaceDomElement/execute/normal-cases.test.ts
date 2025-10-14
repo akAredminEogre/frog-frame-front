412c7fb1fdaaa192faade38c2098090581bd30d4
akAredminEogre <akaredmineogre@gmail.com>
2025-10-15 04:47:36 +0900

    chore: importパスを変更

--- host-frontend-root/frontend-src-root/tests/unit/application/usecases/contextmenu/HandleContextMenuReplaceDomElement/execute/normal-cases.test.ts
+++ host-frontend-root/frontend-src-root/tests/unit/application/usecases/contextmenu/HandleContextMenuReplaceDomElement/execute/normal-cases.test.ts
@@ -3,9 +3,9 @@ import { HandleContextMenuReplaceDomElement } from 'src/application/usecases/con
 import { IChromeTabsService } from 'src/application/ports/IChromeTabsService';
 import { ISelectedPageTextService } from 'src/application/ports/ISelectedPageTextService';
 import { IPopupService } from 'src/application/ports/IPopupService';
-import { createMockTabsService } from 'tests/application/ports/IChromeTabsService/createMockTabsService';
-import { createMockSelectedPageTextService } from 'tests/application/ports/ISelectedPageTextService/createMockSelectedPageTextService';
-import { createMockPopupService } from 'tests/application/ports/IPopupService/createMockPopupService';
+import { createMockTabsService } from 'tests/unit/application/ports/IChromeTabsService/createMockTabsService';
+import { createMockSelectedPageTextService } from 'tests/unit/application/ports/ISelectedPageTextService/createMockSelectedPageTextService';
+import { createMockPopupService } from 'tests/unit/application/ports/IPopupService/createMockPopupService';
 
 /**
  * 1. tabId=123, selection=\"selected text\"での正常処理とサービス実行順序検証
