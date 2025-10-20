8a2e2250a113aa6449019c48e9a7f33f980beff0
akAredminEogre <akaredmineogre@gmail.com>
2025-10-20 10:44:39 +0900

    feat: IRewriteruleRepositoryの抽象クラスのメソッドを追加

--- host-frontend-root/frontend-src-root/src/application/ports/IRewriteRuleRepository.ts
+++ host-frontend-root/frontend-src-root/src/application/ports/IRewriteRuleRepository.ts
@@ -2,7 +2,8 @@ import { RewriteRule } from 'src/domain/entities/RewriteRule/RewriteRule';
 import { RewriteRules } from 'src/domain/value-objects/RewriteRules';
 
 export interface IRewriteRuleRepository {
-  set(rule: RewriteRule): Promise<void>;
+  create(rule: RewriteRule): Promise<void>;
+  update(rule: RewriteRule): Promise<void>;
   getAll(): Promise<RewriteRules>;
   getById(id: number): Promise<RewriteRule>;
 }
